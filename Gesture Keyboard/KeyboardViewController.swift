3d927b4ca8da304e8650ede9525d825e0014b156
Zachary Horine <zachary@horine.dev>
2020-03-17 12:34:38 -0700

    redesign keyboard to use view instead of snapkit

--- Gesture Keyboard/KeyboardViewController.swift
+++ Gesture Keyboard/KeyboardViewController.swift
@@ -7,14 +7,12 @@
 //
 
 import UIKit
-import SnapKit
 import CoreML
 import CoreMotion
 
 class KeyboardViewController: UIInputViewController {
-
-    var caseType: String = ""
-    let keyboardViewHeight = 280
+    
+    var upper: Bool = false;
     
     // MARK:- Properties
     
@@ -35,9 +33,18 @@ class KeyboardViewController: UIInputViewController {
     private var sequenceTargetY: [Double] = []
     private var sequenceTargetZ: [Double] = []
     
+
+
     // MARK:- Outlets
+    @IBOutlet var keyboardView: UIView!
     let SCWidth = UIScreen.main.bounds.width
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var spaceBar: UIButton!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var capsButton: UIButton!
+    @IBOutlet weak var delButton: UIButton!
+    
     
     var pickerData: [String] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     
@@ -50,17 +57,15 @@ class KeyboardViewController: UIInputViewController {
     // MARK:- UIViewControllers
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let heightConstraint = NSLayoutConstraint(item: self.view as Any, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0.0, constant: CGFloat(keyboardViewHeight))
-        self.view.addConstraint(heightConstraint)
-        
-        self.view.backgroundColor = .white
-        
-        caseType = "abc"
-        
-        addNextKeyboardButton()
-        keyboardView()
-        
+        loadInterface();
+        //addNextKeyboardButton()
+    }
+    
+    func loadInterface() {
+        let keyboardNib = UINib(nibName: "KeyboardView", bundle: nil)
+        keyboardView = keyboardNib.instantiate(withOwner: self, options: nil) [0] as! UIView
+        keyboardView.frame.size = view.frame.size
+        view.addSubview(keyboardView)
     }
     
     func addNextKeyboardButton() {
@@ -79,122 +84,7 @@ class KeyboardViewController: UIInputViewController {
         self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
     
-    
-    func keyboardView() {
-        
-        let topView = UIView()
-        topView.backgroundColor = .lightGray
-        view.addSubview(topView)
-        topView.snp.makeConstraints { (make) in
-            make.top.equalTo(view.snp.top)
-            make.left.equalTo(view.snp.left)
-            make.right.equalTo(view.snp.right)
-            make.height.equalTo(50)
-        }
-        
-        //Caps
-        let caseButton: UIButton = UIButton()
-        caseButton.setImage(UIImage(named: "cap.png"), for: .normal)
-        caseButton.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        caseButton.addTarget(self, action: #selector(caseButtonTouch), for: .touchUpInside)
-        topView.addSubview(caseButton)
-        caseButton.snp.makeConstraints { (make) in
-            make.top.equalTo(topView.snp.top)
-            make.left.equalTo(topView.snp.left)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
-        }
-        
-        //Delete
-        let delButton: UIButton = UIButton()
-        delButton.setImage(UIImage(named: "delete.png"), for: .normal)
-        delButton.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        delButton.addTarget(self, action: #selector(delButtonTouch), for: .touchUpInside)
-        topView.addSubview(delButton)
-        delButton.snp.makeConstraints { (make) in
-            make.top.equalTo(topView.snp.top)
-            make.right.equalTo(self.view.snp.right)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
-        }
-        
-        let bottomView = UIView()
-        self.view.addSubview(bottomView)
-        bottomView.snp.makeConstraints { (make) in
-            make.bottom.equalTo(view.snp.bottom)
-            make.left.equalTo(view.snp.left)
-            make.right.equalTo(view.snp.right)
-            make.height.equalTo(50)
-        }
-        
-        //Change Keyboard
-        let chgButton: UIButton = UIButton()
-        chgButton.setImage(UIImage(named: "globe.png"), for: .normal)
-        chgButton.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        chgButton.addTarget(self, action: #selector(chgButtonTouch), for: .touchUpInside)
-        bottomView.addSubview(chgButton)
-        chgButton.snp.makeConstraints { (make) in
-            make.centerY.equalTo(bottomView)
-            make.left.equalTo(bottomView.snp.left).offset(10)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
-        }
-        
-        //Space
-        let spaceButton: UIButton = UIButton()
-//        spaceButton.setImage(UIImage(named: "space.png"), for: .normal)
-//        spaceButton.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        spaceButton.setTitle("Space", for: .normal)
-        spaceButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        spaceButton.layer.cornerRadius = 5
-        spaceButton.layer.masksToBounds = true
-        spaceButton.layer.borderWidth = 4
-        spaceButton.layer.borderColor = UIColor.black.cgColor
-        spaceButton.setTitleColor(.black, for: .normal)
-        spaceButton.addTarget(self, action: #selector(spaceButtonTouch), for: .touchUpInside)
-        bottomView.addSubview(spaceButton)
-        spaceButton.snp.makeConstraints { (make) in
-            make.centerY.equalTo(bottomView)
-            make.left.equalTo(bottomView.snp.left).offset(70)
-            make.right.equalTo(bottomView.snp.right).offset(-70)
-            make.height.equalTo(40)
-        }
-        
-        let keyViewFirst = UIView()
-        keyViewFirst.tag = 2000
-        view.addSubview(keyViewFirst)
-        keyViewFirst.snp.makeConstraints { (make) in
-            make.top.equalTo(topView.snp.bottom).offset(10)
-            make.bottom.equalTo(bottomView.snp.top).offset(-10)
-            make.left.equalTo(view.snp.left)
-            make.right.equalTo(view.snp.right)
-        }
-        let buttonTitles: Array<Any>
-        if caseType == "abc" {
-            //buttonTitles = ["a-z", "a-z", "0-9"]
-            buttonTitles = ["a-z"]
-        }else{
-            //buttonTitles = ["A-Z", "A-Z", "0-9"]
-            buttonTitles = ["A-Z"]
-        }
-        //
-        //let keyFirstWIth = SCWidth / 3
-        let keyFirstWIth = SCWidth
-        for i in 0..<buttonTitles.count {
-            let keyX: CGFloat = CGFloat(i) * keyFirstWIth
-            let keyButton:UIButton = UIButton(frame: CGRect(x: keyX, y: 0, width: keyFirstWIth, height: CGFloat(keyboardViewHeight)-120 ))
-            keyButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-            keyButton.setBackgroundImage(UIImage(named:"frame.png"), for: .normal)
-            keyViewFirst.addSubview(keyButton)
-            keyButton.setTitleColor(.black, for: .normal)
-            keyButton.setTitle((buttonTitles[i] as! String), for: .normal)
-            keyButton.addTarget(self, action: #selector(firstRowButtonTouchUp), for: .touchUpInside)
-            keyButton.addTarget(self, action: #selector(firstRowButtonTouchDown), for: .touchDown)
-        }
-        
-    }
-    
-    @objc func firstRowButtonTouchUp(sender: UIButton) {
+    @IBAction func mainTouchUp(_ sender: Any) {
         motionManager.stopAccelerometerUpdates()
 
         timer.invalidate()
@@ -222,7 +112,7 @@ class KeyboardViewController: UIInputViewController {
         
     }
     
-    @objc func firstRowButtonTouchDown(sender: UIButton) {
+    @IBAction func firstRowButtonTouchDown(sender: UIButton) {
         
         self.sequenceTargetX = []
         self.sequenceTargetY = []
@@ -243,31 +133,26 @@ class KeyboardViewController: UIInputViewController {
         })
     }
     
-    @objc func caseButtonTouch(sender: UIButton) {
-        if caseType == "ABC" {
-            caseType = "abc"
-        }else{
-            caseType = "ABC"
+    @IBAction func caseButtonTouch(_ sender: Any) {
+        if (upper) {
+            upper = false;
+            capsButton.setImage(UIImage(systemName: "capslock"), for: .normal)
+        } else {
+            upper = true;
+            capsButton.setImage(UIImage(systemName: "capslock.fill"), for: .normal)
         }
-        let subViews = self.view.subviews
-        for subview in subViews{
-            if subview.tag == 2000 {
-             subview.removeFromSuperview()
-            }
-        }
-        keyboardView()
     }
     
-    @objc func spaceButtonTouch(sender: UIButton) {
+    @IBAction func spaceButtonTouch(_ sender: Any) {
         let proxy = textDocumentProxy as UITextDocumentProxy
         proxy.insertText(" ")
     }
     
-    @objc func chgButtonTouch(sender: UIButton) {
+    @IBAction func chgButtonTouch(_ sender: Any) {
         self.advanceToNextInputMode()
     }
     
-    @objc func delButtonTouch(sender: UIButton) {
+    @IBAction func delButtonTouch(_ sender: Any) {
         let proxy = textDocumentProxy as UITextDocumentProxy
         proxy.deleteBackward()
     }
